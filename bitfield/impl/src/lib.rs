8bf8c9ed24e4885cd529e3b6878809562424e6f0
Frank King <frankking1729@gmail.com>
2021-01-03 18:16:32 +0800

    FEAT(bitfield): Passed test 02-storage.rs.

--- bitfield/impl/src/lib.rs
+++ bitfield/impl/src/lib.rs
@@ -1,9 +1,33 @@
 use proc_macro::TokenStream;
+use quote::quote;
+use syn::export::Span;
+use syn::{parse_macro_input, ItemStruct, Type};
 
 #[proc_macro_attribute]
 pub fn bitfield(args: TokenStream, input: TokenStream) -> TokenStream {
     let _ = args;
-    let _ = input;
+    let item_struct = parse_macro_input!(input as ItemStruct);
+    let ident = item_struct.ident;
 
-    unimplemented!()
+    let bits: usize = item_struct
+        .fields
+        .iter()
+        .map(|field| {
+            if let Type::Path(path) = &field.ty {
+                if let Some(path) = path.path.segments.last() {
+                    let name = path.ident.to_string();
+                    return name.trim_start_matches('B').parse::<usize>().unwrap_or(0);
+                }
+            }
+            0
+        })
+        .sum();
+    let size = (bits + 7) / 8;
+    let size = syn::LitInt::new(format!("{}_usize", size).as_str(), Span::call_site());
+    TokenStream::from(quote! {
+        #[repr(C)]
+        struct #ident {
+            data: [u8; #size],
+        }
+    })
 }
