3a6f8cf2d1414cc3421af7f1cb27b38cebac6af2
Frank King <frankking1729@gmail.com>
2021-01-05 16:03:57 +0800

    FEAT(builder): Passed test 01-parse.

--- builder/src/lib.rs
+++ builder/src/lib.rs
@@ -1,8 +1,13 @@
 use proc_macro::TokenStream;
 
+use quote::quote;
+use syn::{parse_macro_input, DeriveInput};
+
 #[proc_macro_derive(Builder)]
 pub fn derive(input: TokenStream) -> TokenStream {
-    let _ = input;
-
-    unimplemented!()
+    let input = parse_macro_input!(input as DeriveInput);
+    let ident = input.ident;
+    TokenStream::from(quote! {
+        impl #ident { }
+    })
 }
