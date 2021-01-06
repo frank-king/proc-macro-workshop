3550b5cd58babea3e6dac4fab3ec4cdc591c02eb
Frank King <frankking1729@gmail.com>
2021-01-06 15:08:08 +0800

    FEAT(debug): Passed test 01-parse.

--- debug/src/lib.rs
+++ debug/src/lib.rs
@@ -1,8 +1,18 @@
 use proc_macro::TokenStream;
 
+use quote::quote;
+use syn::{parse_macro_input, DeriveInput};
+
 #[proc_macro_derive(CustomDebug)]
 pub fn derive(input: TokenStream) -> TokenStream {
-    let _ = input;
-
-    unimplemented!()
+    let input = parse_macro_input!(input as DeriveInput);
+    let ident = input.ident;
+    let result = quote! {
+        impl std::fmt::Debug for #ident {
+            fn fmt(&self, fmt: &mut std::fmt::Formatter<'_>) -> std::result::Result<(), std::fmt::Error> {
+                todo!()
+            }
+        }
+    };
+    TokenStream::from(result)
 }
