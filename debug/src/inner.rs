0eed843e359f4e9a659c1f53d539757ad96b5aff
Frank King <frankking1729@gmail.com>
2021-01-06 17:30:34 +0800

    FEAT(debug): Passed test 03-custom-formats.

--- debug/src/inner.rs
+++ debug/src/inner.rs
@@ -1,8 +1,8 @@
 use quote::quote;
-use std::iter::FromIterator;
 use syn::export::TokenStream2;
+use syn::parse::{Parse, ParseStream};
 use syn::spanned::Spanned;
-use syn::{Data, DeriveInput, Error, Field, Fields};
+use syn::{parse2, Data, DeriveInput, Error, Field, Fields, LitStr, Token};
 
 pub struct CustomDebug {
     input: DeriveInput,
@@ -34,28 +34,57 @@ impl CustomDebug {
 
     fn format(&self) -> syn::Result<TokenStream2> {
         match &self.input.data {
-            Data::Struct(r#struct) => Ok(match &r#struct.fields {
+            Data::Struct(r#struct) => match &r#struct.fields {
                 Fields::Named(named) => self.format_fields(named.named.iter()),
                 Fields::Unnamed(unnamed) => self.format_fields(unnamed.unnamed.iter()),
-                Fields::Unit => quote! {},
-            }),
+                Fields::Unit => Ok(quote! {}),
+            },
             Data::Enum(r#enum) => Err(Error::new(r#enum.variants.span(), "enum is not supported")),
             Data::Union(r#union) => Err(Error::new(union.fields.span(), "union is not supported")),
         }
     }
 
-    fn format_fields<'a, Iter>(&'a self, fields: Iter) -> TokenStream2
+    fn format_fields<'a, Iter>(&'a self, fields: Iter) -> syn::Result<TokenStream2>
     where
         Iter: Iterator<Item = &'a Field>,
     {
-        let result =
-            TokenStream2::from_iter(fields.enumerate().map(|(idx, field)| match &field.ident {
-                Some(ident) => quote!(write!(fmt, " {}: {:?},", stringify!(#ident), self.#ident)?;),
-                None => quote!(write!(fmt, " {:?},", self.#idx)?;),
-            }));
-        if result.is_empty() {
-            return result;
+        let mut result = TokenStream2::new();
+        let mut fields = fields.enumerate().peekable();
+        while let Some((idx, field)) = fields.next() {
+            let debug_format = Self::get_debug_format(field)?;
+            let punct = if fields.peek().is_some() { ',' } else { ' ' };
+            let format = match &field.ident {
+                Some(ident) => {
+                    let format = format!(" {{}}: {}{}", debug_format, punct);
+                    quote!(write!(fmt, #format, stringify!(#ident), self.#ident)?;)
+                }
+                None => {
+                    let format = format!(" {}{}", debug_format, punct);
+                    quote!(write!(fmt, #format, self.#idx)?;)
+                }
+            };
+            result.extend(format);
         }
-        quote!(#result write!(fmt, " ")?; )
+        Ok(result)
+    }
+
+    fn get_debug_format(field: &Field) -> syn::Result<String> {
+        if field.attrs.is_empty() {
+            return Ok("{:?}".to_owned());
+        }
+        if field.attrs.len() > 1 {
+            return Err(Error::new(field.span(), "Too much attributes"));
+        }
+        let attr = &field.attrs[0];
+        struct Format(LitStr);
+        impl Parse for Format {
+            fn parse(input: ParseStream) -> syn::Result<Self> {
+                let _eq: Token![=] = input.parse()?;
+                let format = input.parse()?;
+                Ok(Format(format))
+            }
+        }
+        let format: Format = parse2(attr.tokens.clone())?;
+        Ok(format.0.value())
     }
 }
--- debug/src/lib.rs
+++ debug/src/lib.rs
@@ -5,7 +5,7 @@ use syn::{parse_macro_input, DeriveInput};
 
 mod inner;
 
-#[proc_macro_derive(CustomDebug)]
+#[proc_macro_derive(CustomDebug, attributes(debug))]
 pub fn derive(input: TokenStream) -> TokenStream {
     let input = parse_macro_input!(input as DeriveInput);
     let custom_debug = CustomDebug::from_derive_input(input);
